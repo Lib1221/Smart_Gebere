69f7d705918433dc80e7bfd377904a4aa3469196
Liben <adugnaliben65@gmail.com>
2025-01-27 21:30:21 +0300

    🔧✨ Refactor analysis_options.yaml: ignore library_private_types_in_public_api, unused_import, deprecated_member_use, and prefer_final_fields 📄🔄 Update lib/Disease_page/api_service.dart 🔐🔄 Update lib/auth/login/login.dart 📝🔄 Update lib/auth/signup/sign_up.dart

--- lib/auth/login/login.dart
+++ lib/auth/login/login.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -274,7 +276,7 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
     });
 
     try {
-      UserCredential userCredential =
+
           await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: _usernameController.text,
         password: _passwordController.text,
