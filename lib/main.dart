84c3669a77351e620bef17eae2db1a20751d784c
Liben <adugnaliben65@gmail.com>
2025-02-14 01:52:21 +0300

    Refactor main.dart and update app entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:smart_gebere/Disease_page/DiseaseDetection.dart';
-import 'package:smart_gebere/Home/Home.dart';
+
+
 import 'package:firebase_core/firebase_core.dart';
-import 'package:smart_gebere/Loading/loading.dart';
-import 'package:smart_gebere/auth/login/login.dart';
-import 'package:smart_gebere/off_track/list_generate.dart';
-import 'package:smart_gebere/stream/stream_provider.dart';
-import 'firebase_options.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:smart_gebere/firebase_options.dart';
+import 'package:smart_gebere/stream/stream_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,10 +14,8 @@ void main() async {
   );
   runApp(
     MaterialApp(
-      home:CropSuggestionPage(), //StreamProviderClass()
+      debugShowCheckedModeBanner: false,
+      home:StreamProviderClass(),
     )
     );
 }
-
-
-
