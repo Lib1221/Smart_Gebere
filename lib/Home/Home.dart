01b84670d2143cfedb64869b4d029927a9dc0257
Liben <adugnaliben65@gmail.com>
2025-02-18 00:16:30 +0300

    Refactor Home_Screen to use consistent colors and styles for task sections

--- lib/Home/Home.dart
+++ lib/Home/Home.dart
@@ -36,7 +36,7 @@ class Home_Screen extends StatelessWidget {
         height: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Colors.white, Colors.grey.shade200],
+            colors: [Colors.white, Colors.green.shade50],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
@@ -44,78 +44,85 @@ class Home_Screen extends StatelessWidget {
         child: SafeArea(
           child: Column(
             children: [
-              // Body
               Expanded(
                 child: Column(
                   children: [
-                    // Create Task Section
                     Flexible(
                       flex: 1,
                       child: Container(
                         margin: const EdgeInsets.all(10),
                         padding: const EdgeInsets.all(10),
-                        color: Colors.deepPurple.shade50,
+                        decoration: BoxDecoration(
+                          color: Colors.green.shade50,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              offset: const Offset(0, 4),
+                              blurRadius: 6,
+                              spreadRadius: 1,
+                            ),
+                          ],
+                        ),
                         child: Column(
                           children: [
-                            SectionHeader(title: 'Create Task', bgColor: Colors.deepPurple.shade100),
+                            SectionHeader(title: 'Create Task', bgColor: Colors.green.shade100),
                             const TaskCreationSection(),
                           ],
                         ),
                       ),
                     ),
-                    // Created Tasks Section
-                    
+
                     const SizedBox(height: 10),
                     Flexible(
                       flex: 1,
                       child: Container(
                         margin: const EdgeInsets.all(10),
                         padding: const EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                color: Colors.teal.shade50,
-                                borderRadius: BorderRadius.circular(10), 
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black.withOpacity(0.1), 
-                                    offset: const Offset(0, 4), 
-                                    blurRadius: 6, 
-                                    spreadRadius: 1, 
-                                  ),
-                                ],
-                              ),
-                              child: Column(
-                                children: [
-                                  SectionHeader(title: 'Created Tasks', bgColor: Colors.teal.shade100),
-                                  const SizedBox(height: 10,),
-                                  Expanded(child: SlideableCreatedTasks()),
-                                ],
-                              ),
+                        decoration: BoxDecoration(
+                          color: Colors.green.shade50,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              offset: const Offset(0, 4),
+                              blurRadius: 6,
+                              spreadRadius: 1,
                             ),
-                                                ),
+                          ],
+                        ),
+                        child: Column(
+                          children: [
+                            SectionHeader(title: 'Created Tasks', bgColor: Colors.green.shade100),
+                            const SizedBox(height: 10),
+                            Expanded(child: SlideableCreatedTasks()),
+                          ],
+                        ),
+                      ),
+                    ),
+
                     const SizedBox(height: 10),
                     Flexible(
                       flex: 1,
                       child: Container(
                         decoration: BoxDecoration(
-                                color: Colors.amber.shade50,
-                                borderRadius: BorderRadius.circular(10), 
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black.withOpacity(0.1), 
-                                    offset: const Offset(0, 4), 
-                                    blurRadius: 6, 
-                                    spreadRadius: 1, 
-                                  ),
-                                ],
-                              ),
+                          color: Colors.green.shade50,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.1),
+                              offset: const Offset(0, 4),
+                              blurRadius: 6,
+                              spreadRadius: 1,
+                            ),
+                          ],
+                        ),
                         margin: const EdgeInsets.all(10),
                         padding: const EdgeInsets.all(10),
-                        
                         child: Column(
                           children: [
-                            SectionHeader(title: 'Expected Events', bgColor: Colors.amber.shade100),
-                            const SizedBox(height: 10,),
-
+                            SectionHeader(title: 'Expected Events', bgColor: Colors.green.shade100),
+                            const SizedBox(height: 10),
                             const Expanded(child: SlideableExpectedEvents()),
                           ],
                         ),
@@ -132,7 +139,6 @@ class Home_Screen extends StatelessWidget {
   }
 }
 
-// Section Header
 class SectionHeader extends StatelessWidget {
   final String title;
   final Color bgColor;
@@ -144,7 +150,10 @@ class SectionHeader extends StatelessWidget {
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.all(12),
-      color: bgColor,
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
       child: Text(
         title,
         style: const TextStyle(
