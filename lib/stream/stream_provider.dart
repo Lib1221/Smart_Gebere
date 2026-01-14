81af0bc464cae969b56d66b7ecedfa5dab5b082d
Henok-Demissie <101617532+Henok-Demissie@users.noreply.github.com>
2026-01-14 23:34:40 +0400

    Implement AuthService and StreamProviderClass

--- lib/stream/stream_provider.dart
+++ lib/stream/stream_provider.dart
@@ -1,1 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:smart_gebere/splash/splash_screen.dart';
+
+class UserModel {
+  final String uid;
+  final String? email;
+  final String? displayName;
+
+  UserModel({required this.uid, this.email, this.displayName});
+}
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Convert Firebase User to UserModel
+  UserModel? _userFromFirebaseUser(User? user) {
+    return user != null
+        ? UserModel(
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName,
+          )
+        : null;
+  }
+
+  // Auth Stream to listen for changes
+  Stream<UserModel?> get user {
+    return _auth.authStateChanges().map(_userFromFirebaseUser);
+  }
+}
+
+class StreamProviderClass extends StatelessWidget {
+  const StreamProviderClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<UserModel?>.value(
+      value: AuthService().user,
+      initialData: null,
+      child: const SplashScreen(),
+    );
+  }
+}
 
