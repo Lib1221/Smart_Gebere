e906fc602a796fd30ae0944b2c23783f07236f7f
Abel <abelgetahun66@gmail.com>
2026-01-18 12:00:00 +0300

    image string utils

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,13 @@
+// app images in string format
+class TImages{
+
+  //onboarding images - lottie files
+  static const String onBoardingImage1 = 'assets/images/on_boarding_images/page_1.json';
+  static const String onBoardingImage2 = 'assets/images/on_boarding_images/page_2.json';
+  static const String onBoardingImage3 = 'assets/images/on_boarding_images/page_3.json';
+
+  //login 
+  static const String login_background = "assets/images/auth_images/login_background.jpg"; // Replace with the correct path to your image
+  static const String login_background_dark = "assets/images/auth_images/login_background_dark.jpg";
+  
+}
